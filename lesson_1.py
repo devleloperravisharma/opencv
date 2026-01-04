c8a57457d3469c0b0f05401c84eedb0b009fa2cc
devleloperravisharma <47600036+devleloperravisharma@users.noreply.github.com>
2026-01-04 12:58:10 -0600

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,5 @@
+import cv2
+import os
+img=cv2.imread("C:/Users/Ravis/Documents/Riddhima/open CV/opencvclass1.jpg",cv2.IMREAD_COLOR)
+cv2.imshow("Background Image",img)#colored image
+cv2.waitKey(0)  # waits for a key to be pressed in order to close it
