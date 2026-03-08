c7e165982afbf357d2a1a06621ef4e5fb82a3c7b
devleloperravisharma <47600036+devleloperravisharma@users.noreply.github.com>
2026-03-08 13:35:10 -0500

    Add files via upload

--- lesson6.py
+++ lesson6.py
@@ -21,7 +21,18 @@ while (videoo.isOpened()):
     upper_red = np.array([100, 255, 255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
     lower_red = np.array([155, 40, 40])
-    upper_red = np.arry([180, 255, 255])
+    upper_red = np.array([180, 255, 255])
     mask2 = cv2.inRange(hsv, lower_red, upper_red)
     mask1 = mask1 + mask2
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3)), iterations = 2)
+    mask1 = cv2.dilate(mask1, np.ones((3,3)), iterations = 1)
+    mask2 = cv2.bitwise_not(mask1)
+    result1 = cv2.bitwise_and(storing_bg_img, storing_bg_img, mask = mask1)
+    result2 = cv2.bitwise_and(img, img, mask = mask2)
+    final_result = cv2.addWeighted(result1, 1, result2, 1, 0)
+    cv2.imshow("invisible man !!!!!", final_result)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+
     
